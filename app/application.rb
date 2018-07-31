9d38ac5648a69d9d7aef7a4032f39ebb652e692d
Jamie Brown <jmbrowncow@gmail.com>
2018-07-31 19:15:26 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,19 @@
+class Application
+  def call(env)
+    resp = Rack::Response.new
+
+    time = Time.new
+
+    resp.write "#{time}"
+    # if num_1==num_2 && num_2==num_3
+    #   resp.write "#{num_1}, #{num_2}, #{num_3} \n"
+    #   resp.write "You Win"
+    # else
+    #   resp.write "#{num_1}, #{num_2}, #{num_3} \n"
+    #   resp.write "You Lose"
+    # end
+
+    resp.finish
+  end
+end
+end
