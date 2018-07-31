9f82e1d22a9774ea55f4b08eeddaf8e8545d357c
Jamie Brown <jmbrowncow@gmail.com>
2018-07-31 19:22:02 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    time = Time.new.hour.to_s
+    time = Time.new.to_s
     less_date = time.split(" ")[1]
     resp.write "#{less_date}"
     # if num_1==num_2 && num_2==num_3
