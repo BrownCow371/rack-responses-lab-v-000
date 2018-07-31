8c99264874304e97b2a5b32f2caf222e90b9bd6a
Jamie Brown <jmbrowncow@gmail.com>
2018-07-31 19:21:19 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,9 +2,9 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    time = Time.new.hour
-
-    resp.write "#{time}"
+    time = Time.new.hour.to_s
+    less_date = time.split(" ")[1]
+    resp.write "#{less_date}"
     # if num_1==num_2 && num_2==num_3
     #   resp.write "#{num_1}, #{num_2}, #{num_3} \n"
     #   resp.write "You Win"
